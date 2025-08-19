f25afed7e714ec3e442ce532f7c539ec72d362f9
StreetShiffter <agwzdushaz@gmail.com>
2025-08-19 08:29:46 +0700

    Update views.py

--- users/views.py
+++ users/views.py
@@ -10,19 +10,23 @@ from django.views import View
 from django.urls import reverse_lazy
 
 class UserRegisterView(View):
+    '''Контроллер регистрации'''
     form_class = CustomUserCreationForm
     template_name = 'register.html'
     success_url = reverse_lazy('home')
 
 class UserProfileView(View):
+    '''Контроллер отображение профиля'''
     template_name = 'profile.html'
 
 
 class CustomLoginView(LoginView):
+    '''Контроллер входа в профиль'''
     template_name = 'loging.html'
     success_url = reverse_lazy('users:profile')
 
 class CustomLogoutView(View):
+    '''Контроллер выхода из профиля'''
     def get(self, request):
         logout(request)
         messages.add_message(request, messages.INFO, 'Вы успешно вышли.')
