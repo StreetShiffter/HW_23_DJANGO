5fe4c584afa1b090c7d9676381b7bd1d7794a9d2
StreetShiffter <agwzdushaz@gmail.com>
2025-08-23 12:35:28 +0700

    Fix models user

--- users/models.py
+++ users/models.py
@@ -1,7 +1,27 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import EmailValidator, MaxLengthValidator
 from django.db import models
 
+# 1. Сначала — кастомный менеджер
+class UserManager(BaseUserManager):
+    '''Кастомное правило создание пользователя и суперпользователя'''
+    def create_user(self, email, password=None, **extra_fields):
+        # Проверяем, что email передан
+        if not email:
+            raise ValueError('Email обязателен')
+        email = self.normalize_email(email)# MrJakcon@YanDex.Ru > MrJakcon@yandex.ru
+        user = self.model(email=email, **extra_fields)# ← Создаём объект пользователя
+        user.set_password(password)# Создаем пароль и хэшируем для безопасности
+        user.save(using=self._db)# Сохраняем пользователя и даем атрибут если у вас несколько БД
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+# 2. Потом — сама модель пользователя
 class User(AbstractUser):
     '''Кастомная модель пользователя'''
     username = None
@@ -13,6 +33,16 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    # Подключаем наш кастомный менеджер → теперь Django не строит объект из модели, а берет "правило"
+    objects = UserManager()
+
+#Метод приcваивания username поле email - для корректного создания пользователя через django shell
+    #Работает без UserManager, но путает, т.к. теперь две одинаковые записи
+    # def save(self, *args, **kwargs):
+    #     if not self.username:
+    #         self.username = self.email  # делаем username = email
+    #     super().save(*args, **kwargs)
 
     def __str__(self):
         return self.email
