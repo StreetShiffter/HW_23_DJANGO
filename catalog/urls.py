0d20f516725de5bf4c2212726b6fa985cf98accb
StreetShiffter <agwzdushaz@gmail.com>
2025-08-26 21:08:20 +0700

    Add cache detail view product in id key

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from .views import HomeListView, ProductDetailView, FeedbackView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('product/<int:pk>/', ProductDetailView.as_view(), name="product"),
+    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name="product"),
     path('contacts/',FeedbackView.as_view(), name="contacts"),
     path('', HomeListView.as_view(), name="home"),
     path('product/create/', ProductCreateView.as_view(), name="create"),
